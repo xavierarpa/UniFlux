c3feab83c557a5ef8bd3f2a60b7d3368d9c51386
Kingdox ~ Xavier Arpa ~ <arpaxavier@gmail.com>
2023-03-19 20:25:38 +0100

    Update FluxTest.cs

--- Test/FluxTest.cs
+++ Test/FluxTest.cs
@@ -24,92 +24,95 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using Kingdox.Flux;
-public class FluxTest : MonoBehaviour
+namespace Kingdox.Flux.Test
 {
-    public const string OnWait = "OnWait";
-    private async void Start()
+    public class FluxTest : MonoBehaviour
     {
-        //
-        // NORMAL
-        //
-        OnWait.Subscribe(Method,true); // Method
-        OnWait.Subscribe<string>(MethodParam,true); // Method Param
-        OnWait.Subscribe<string>(MethodReturn,true); // Method Return
-        OnWait.Subscribe<string, string>(MethodParamReturn,true); // Method Param Return
-        OnWait.Invoke();
-        OnWait.Invoke<string>("MethodParam");
-        Debug.Log(OnWait.Invoke<string>());
-        Debug.Log(OnWait.Invoke<string, string>("MethodParamReturn"));
-        //
-        // IENUMERATOR
-        //
-        OnWait.Subscribe(Yield, true); // Method
-        OnWait.Subscribe<string, IEnumerator>(YieldParam, true); // Method Param
-        OnWait.Subscribe<IEnumerator<string>>(YieldReturn, true); // Method Return
-        OnWait.Subscribe<string, IEnumerator<string>>(YieldParamReturn, true); // Method Param Return
-        StartCoroutine(OnWait.Invoke<IEnumerator>());
-        StartCoroutine(OnWait.Invoke<string, IEnumerator>("a"));
-        StartCoroutine(OnWait.Invoke<IEnumerator<string>>());
-        StartCoroutine(OnWait.Invoke<string, IEnumerator<string>>("a"));
-        //
-        // TASK
-        //
-        OnWait.Subscribe(Await, true); // Method
-        OnWait.Subscribe<string, Task>(AwaitParam, true); // Method Param
-        OnWait.Subscribe<Task<string>>(AwaitReturn, true); // Method Return
-        OnWait.Subscribe<string, Task<string>>(AwaitParamReturn, true); // Method Param Return
-        await OnWait.Invoke<Task>();
-        await OnWait.Invoke<string, Task>("a");
-        await OnWait.Invoke<Task<string>>();
-        await OnWait.Invoke<string, Task<string>>("a");
-    }
+        public const string OnWait = "OnWait";
+        private async void Start()
+        {
+            //
+            // NORMAL
+            //
+            OnWait.Subscribe(Method,true); // Method
+            OnWait.Subscribe<string>(MethodParam,true); // Method Param
+            OnWait.Subscribe<string>(MethodReturn,true); // Method Return
+            OnWait.Subscribe<string, string>(MethodParamReturn,true); // Method Param Return
+            OnWait.Invoke();
+            OnWait.Invoke<string>("MethodParam");
+            Debug.Log(OnWait.Invoke<string>());
+            Debug.Log(OnWait.Invoke<string, string>("MethodParamReturn"));
+            //
+            // IENUMERATOR
+            //
+            OnWait.Subscribe(Yield, true); // Method
+            OnWait.Subscribe<string, IEnumerator>(YieldParam, true); // Method Param
+            OnWait.Subscribe<IEnumerator<string>>(YieldReturn, true); // Method Return
+            OnWait.Subscribe<string, IEnumerator<string>>(YieldParamReturn, true); // Method Param Return
+            StartCoroutine(OnWait.Invoke<IEnumerator>());
+            StartCoroutine(OnWait.Invoke<string, IEnumerator>("a"));
+            StartCoroutine(OnWait.Invoke<IEnumerator<string>>());
+            StartCoroutine(OnWait.Invoke<string, IEnumerator<string>>("a"));
+            //
+            // TASK
+            //
+            OnWait.Subscribe(Await, true); // Method
+            OnWait.Subscribe<string, Task>(AwaitParam, true); // Method Param
+            OnWait.Subscribe<Task<string>>(AwaitReturn, true); // Method Return
+            OnWait.Subscribe<string, Task<string>>(AwaitParamReturn, true); // Method Param Return
+            await OnWait.Invoke<Task>();
+            await OnWait.Invoke<string, Task>("a");
+            await OnWait.Invoke<Task<string>>();
+            await OnWait.Invoke<string, Task<string>>("a");
+        }
 
-    private void Method() => Debug.Log("Method");
-    private void MethodParam(string a) => Debug.Log(a);
-    private string MethodReturn() => "MethodReturn";
-    private string MethodParamReturn(string a) => a;
+        private void Method() => Debug.Log("Method");
+        private void MethodParam(string a) => Debug.Log(a);
+        private string MethodReturn() => "MethodReturn";
+        private string MethodParamReturn(string a) => a;
 
-    private IEnumerator Yield() 
-    {
-        Debug.Log("Yield");
-        yield return null;
-    }
-    private IEnumerator YieldParam(string a) 
-    {
-        Debug.Log("YieldParam");
-        yield return null;
-    }
-    private IEnumerator<string> YieldReturn() 
-    {
-        Debug.Log("YieldReturn");
-        yield return null;
-    }
-    private IEnumerator<string> YieldParamReturn(string data) 
-    {
-        Debug.Log("YieldParamReturn");
-        yield return null;
-    }
+        private IEnumerator Yield() 
+        {
+            Debug.Log("Yield");
+            yield return null;
+        }
+        private IEnumerator YieldParam(string a) 
+        {
+            Debug.Log("YieldParam");
+            yield return null;
+        }
+        private IEnumerator<string> YieldReturn() 
+        {
+            Debug.Log("YieldReturn");
+            yield return null;
+        }
+        private IEnumerator<string> YieldParamReturn(string data) 
+        {
+            Debug.Log("YieldParamReturn");
+            yield return null;
+        }
 
-    private async Task Await() 
-    {
-        Debug.Log("Await");
-        await Task.Yield();
-    }
-    private async Task AwaitParam(string a) 
-    {
-        Debug.Log("AwaitParam");
-        await Task.Yield();
-    }
-    private async Task<string> AwaitReturn() 
-    {
-        Debug.Log("AwaitReturn");
-        await Task.Yield();
-        return "a";
-    }
-    private async Task<string> AwaitParamReturn(string data) 
-    {
-        Debug.Log("AwaitParamReturn");
-        await Task.Yield();
-        return "a";
+        private async Task Await() 
+        {
+            Debug.Log("Await");
+            await Task.Yield();
+        }
+        private async Task AwaitParam(string a) 
+        {
+            Debug.Log("AwaitParam");
+            await Task.Yield();
+        }
+        private async Task<string> AwaitReturn() 
+        {
+            Debug.Log("AwaitReturn");
+            await Task.Yield();
+            return "a";
+        }
+        private async Task<string> AwaitParamReturn(string data) 
+        {
+            Debug.Log("AwaitParamReturn");
+            await Task.Yield();
+            return "a";
+        }
     }
 }
