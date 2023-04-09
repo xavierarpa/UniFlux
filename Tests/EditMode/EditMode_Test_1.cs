adbca4ee0ede6fab1340c481e8e22f5a54e1dbe3
Xavier Arpa <arpaxavier@gmail.com>
2023-04-09 04:08:35 +0200

    -

--- Tests/EditMode/EditMode_Test_1.cs
+++ Tests/EditMode/EditMode_Test_1.cs
@@ -1,25 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using Kingdox.UniFlux;
+using Kingdox.UniFlux.Core;
 
 public class EditMode_Test_1
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void EditMode_Test_1SimplePasses()
+    [Test] public void EditMode_Test_2_UniFlux_Subscribe_Pass()
     {
-        // Use the Assert class to test conditions
+        //Subscribe
+        Flux.Store("OnTest", OnTest_Action, true); // Action
+        Flux.Store<string,bool>("OnTest", OnTest_ActionParam, true); // ActionParam
+        Flux.Store<string, int>("OnTest", OnTest_Func, true); // Func
+        Flux.Store<string,float,float>("OnTest", OnTest_FuncPatam, true); // FuncParam
     }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator EditMode_Test_1WithEnumeratorPasses()
+    [Test] public void EditMode_Test_3_UniFlux_Action_Pass()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        //Action
+        Flux.Dispatch("OnTest"); // Action
+        Flux.Dispatch("OnTest", true); // ActionParamal
+        var val = Flux.Dispatch<string,int>("OnTest"); // Func
+        var val_2 = Flux.Dispatch<string, float, float>("OnTest", 3f); // FuncParam
+    }
+    [Test] public void EditMode_Test_4_UniFlux_Unsubscribe_Pass()
+    {
+        //Unsubscribe
+        Flux.Store("OnTest", OnTest_Action, false); // Action
+        Flux.Store<string, bool>("OnTest", OnTest_ActionParam, false); // ActionParam
+        Flux.Store<string, int>("OnTest", OnTest_Func, false); // Func
+        Flux.Store<string, float, float>("OnTest", OnTest_FuncPatam, false); // FuncParam
+    }
+    public void OnTest_Action() 
+    {
+        Debug.Log("OnTest");
+    }
+    public void OnTest_ActionParam(bool condition) 
+    {
+        Debug.Log($"ActionParam: {condition}");
+    }
+    public int OnTest_Func() 
+    {
+        return 42;
+    }
+    public float OnTest_FuncPatam(float value) 
+    {
+        return value;
     }
 }
