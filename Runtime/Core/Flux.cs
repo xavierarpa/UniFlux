9980de8da81153902bcfd228fa76a77afb0ccb2a
Xavier Arpa <arpaxavier@gmail.com>
2023-04-23 13:21:51 +0200

    - Experiment with Scrptable Object as Key Store, allowing modularity of keys

--- Runtime/Core/Flux.cs
+++ Runtime/Core/Flux.cs
@@ -1,5 +1,4 @@
 using System;
-
 namespace Kingdox.UniFlux.Core
 {
     public static class Flux 
---
+++ Runtime/_Experimental/Sample/UniFlux_Exp_S_1_C.cs
@@ -0,0 +1,31 @@
+/*
+Copyright (c) 2023 Xavier Arpa López Thomas Peter ('Kingdox')
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using UnityEngine;
+namespace Kingdox.UniFlux.Experimental
+{
+    public class UniFlux_Exp_S_1_C : MonoFlux
+    {
+        [SerializeField] KeyFlux k_onSample;
+        protected override void OnFlux(in bool condition) => k_onSample.Store(OnSample, condition);
+        private void OnSample() => Debug.Log("On Sample!");   
+    }
+}
