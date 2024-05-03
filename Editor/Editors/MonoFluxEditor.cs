9d427e9ba9f27500253d1ee29b4f5ab726870a53
Xavier Arpa <arpaxavier@gmail.com>
2024-05-03 03:28:01 +0200

    - inspector full mode

--- Editor/Editors/MonoFluxEditor.cs
+++ Editor/Editors/MonoFluxEditor.cs
@@ -26,6 +26,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using UniFlux.Core;
+using Unity.Properties;
 
 namespace UniFlux.Editor
 {
@@ -33,8 +34,14 @@ namespace UniFlux.Editor
     public partial class MonoFluxEditor : UnityEditor.Editor
     {
         private MethodInfo[] methods_subscribeAttrb;
-        private Dictionary<MethodInfo, object[]> dic_method_parameters;
-        private static bool showBox = true;
+        private Dictionary<MethodInfo, object> dic_method_parameters;
+        private Dictionary<MethodInfo, object> dic_method_outputs;
+        public object obj_input;
+        private static bool ShowMethods
+        { 
+            get => PlayerPrefs.GetInt("__UniFlux.MonoFluxEditor.ShowBox", default) == default;
+            set => PlayerPrefs.SetInt("__UniFlux.MonoFluxEditor.ShowBox", value ? default : 1);
+        }
         private void OnEnable()
         {
             Type type = target.GetType();
@@ -42,7 +49,8 @@ namespace UniFlux.Editor
             #pragma warning disable CS0618
             methods_subscribeAttrb = methods.Where(m => m.GetCustomAttributes(typeof(FluxAttribute), true).Length > 0).ToArray();
             #pragma warning restore CS0618
-            dic_method_parameters = methods_subscribeAttrb.Select(m => new { Method = m, Parameters = new object[m.GetParameters().Length] }).ToDictionary(mp => mp.Method, mp => mp.Parameters);
+            dic_method_parameters = methods_subscribeAttrb.Select(m => new { Method = m, Parameters = null as object  }).ToDictionary(mp => mp.Method, mp => mp.Parameters);
+            dic_method_outputs = methods_subscribeAttrb.Select(m => new { Method = m, Return = null as object }).ToDictionary(mp => mp.Method, mp => mp.Return);
         }
         public override void OnInspectorGUI()
         {
@@ -50,16 +58,16 @@ namespace UniFlux.Editor
 
             if(methods_subscribeAttrb.Length.Equals(0))
             {
-                showBox = false;
+                ShowMethods = false;
             }
             else
             {
-                if(GUILayout.Button( showBox ? "Close" : $"Open ({methods_subscribeAttrb.Length})", GUI.skin.box))
+                if(GUILayout.Button( ShowMethods ? "Close" : $"Open ({methods_subscribeAttrb.Length})", GUI.skin.box))
                 {
-                    showBox = !showBox;
+                    ShowMethods = !ShowMethods;
                 }
             }
-            if(showBox)
+            if(ShowMethods)
             {
                 _Draw();
             }
@@ -85,105 +93,92 @@ namespace UniFlux.Editor
                 #pragma warning restore CS0618
                 var parameters = item.GetParameters();
                 var isParameters = parameters.Length > 0;
-                var isErr_return = item.ReturnType != typeof(void);
+                var isReturn = item.ReturnType != typeof(void);
                 var isErr_static = item.IsStatic;
-                var isError = isParameters || isErr_return || isErr_static;
+                var isError = isErr_static;
                 string key_color =  isError ? "yellow" : "white";
+                
+                int opt_status = (param: isParameters, rtrn: isReturn) switch
+                {
+                    (false, false) => 0, // Action
+                    (true, false) => 1, // Action<T>
+                    (false, true) => 2, // Func<T>
+                    (true, true) => 3, // Func<T, T2>
+                };
+
+                //
                 EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                // KEY
                 GUILayout.Label( $"<color={key_color}>{atribute.key}</color>", style_title);
+                // Method Name
                 GUILayout.Label(item.ToString(), EditorStyles.whiteMiniLabel);
-                GenerateButton(buttonStyle,item);
+
+                if(isParameters)
+                {
+                    // INPUT
+                    GenerateInput(item, parameters);
+                }
+
+                // INVOKE BUTTON
+                GenerateButton(buttonStyle, item, opt_status);
+
+                if(isReturn)
+                {
+                    // OUTPUT
+                    GenerateOutput(item, parameters);
+                }
+
                 EditorGUILayout.BeginVertical();
-                GenerateParameters(item, parameters);
                 EditorGUILayout.EndVertical();
                 EditorGUILayout.EndVertical();
                 GUILayout.Space(5);
             }
         }
-        // private void GenerateParameters(MethodInfo item, ParameterInfo[] parameters)
-        // {
-        //     // var args = dic_method_parameters[item];
-        //     // for (int i = 0; i < parameters.Length; i++)
-        //     // {
-        //     //     EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
-        //     //     GUILayout.Label(parameters[i].ToString());
-        //     //     EditorGUILayout.EndHorizontal();
-        //     // }
-        //     // dic_method_parameters[item] = args;
-        // }
-        private void GenerateParameters(MethodInfo item, ParameterInfo[] parameters)
-        {
-            // var args = dic_method_parameters[item];
-
-            // for (int i = 0; i < parameters.Length; i++)
-            // {
-            //     var parameter = parameters[i];
-
-            //     if (parameter.ParameterType.IsValueType)
-            //     {
-            //         // El parámetro es primitivo
-            //         object defaultValue = Activator.CreateInstance(parameter.ParameterType);
-            //         object value = args[i] ?? defaultValue;
-
-            //         EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
-            //         GUILayout.Label(parameter.Name, GUILayout.Width(150));
-
-            //         if (parameter.ParameterType == typeof(int))
-            //         {
-            //             value = EditorGUILayout.IntField((int)value);
-            //         }
-            //         else if (parameter.ParameterType == typeof(float))
-            //         {
-            //             value = EditorGUILayout.FloatField((float)value);
-            //         }
-            //         else if (parameter.ParameterType == typeof(bool))
-            //         {
-            //             value = EditorGUILayout.Toggle((bool)value);
-            //         }
-            //         else if (parameter.ParameterType == typeof(string))
-            //         {
-            //             value = EditorGUILayout.TextField((string)value);
-            //         }
-            //         else if (parameter.ParameterType == typeof(Vector2))
-            //         {
-            //             value = EditorGUILayout.Vector2Field("", (Vector2)value);
-            //         }
-            //         else if (parameter.ParameterType == typeof(Vector3))
-            //         {
-            //             value = EditorGUILayout.Vector3Field("", (Vector3)value);
-            //         }
-            //         else if (parameter.ParameterType == typeof(Vector4))
-            //         {
-            //             value = EditorGUILayout.Vector4Field("", (Vector4)value);
-            //         }
-
-            //         args[i] = Convert.ChangeType(value, parameter.ParameterType);
-            //         EditorGUILayout.EndHorizontal();
-            //     }
-            //     else
-            //     {
-            //         // El parámetro no es primitivo
-            //         GUILayout.Label(parameter.Name + " (no primitivo)", EditorStyles.whiteMiniLabel);
-            //     }
-            // }
-
-            // dic_method_parameters[item] = args;
-        }
-
-        private void GenerateButton(GUIStyle buttonStyle, MethodInfo item)
+        private void GenerateInput(MethodInfo item, ParameterInfo[] parameters)
         {
             GUI.enabled = Application.isPlaying;
-            if (dic_method_parameters[item].Length.Equals(0) && GUILayout.Button($"Invoke!", buttonStyle)) 
+            dic_method_parameters[item] = GUILayouts.SuperField(item.GetParameters()[0].Name, parameters[0].ParameterType, dic_method_parameters[item]);
+            GUI.enabled = true;
+        }
+        private void GenerateOutput(MethodInfo item, ParameterInfo[] parameters)
+        {
+            GUI.enabled = false;
+            dic_method_outputs[item] = GUILayouts.SuperField($"Output: {item.ReturnType}", item.ReturnType, dic_method_outputs[item]);
+            GUI.enabled = true;
+        }
+
+        private void GenerateButton(GUIStyle buttonStyle, MethodInfo item, int opt)
+        {
+            GUI.enabled = Application.isPlaying;
+
+            switch (opt)
             {
-                if(item.ReturnType != typeof(void))
-                {
-                    var resp = item.Invoke(target, dic_method_parameters[item]);
-                    Debug.Log(resp);
-                }
-                else
-                {
-                    item.Invoke(target, dic_method_parameters[item]);
-                }
+                case 0: // Action
+                    if(GUILayout.Button($"Invoke!", buttonStyle))
+                    {
+                        item.Invoke(target, new object[0]);
+                    }
+                break;
+                case 1: // Action<T>
+                    if(GUILayout.Button($"Invoke!", buttonStyle))
+                    {
+                        item.Invoke(target, new object[]{dic_method_parameters[item]});
+                    }
+                break;
+                case 2: // Func<T>
+                    if(GUILayout.Button($"Invoke!", buttonStyle))
+                    {
+                        var resp_2 = item.Invoke(target, new object[0]);
+                        dic_method_outputs[item] = resp_2;
+                    }
+                break;
+                case 3: // Func<T, T2>
+                    if(GUILayout.Button($"Invoke!", buttonStyle))
+                    {
+                        var resp_3 = item.Invoke(target, new object[]{dic_method_parameters[item]});
+                        dic_method_outputs[item] = resp_3;
+                    }
+                break;
             }
             GUI.enabled = true;
         }
--- Editor/GUILayouts/UniFluxGUILayouts.cs
+++ Editor/GUILayouts/UniFluxGUILayouts.cs
@@ -1,13 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace UniFlux.Editor
 {
     public static class GUILayouts
     {
+        private static readonly Dictionary<Type, Func<string, object, object>> editorFieldMap = new Dictionary<Type, Func<string, object, object>>
+        {
+            { typeof(float), (title, value) => EditorGUILayout.FloatField(title, (float)value) },
+            { typeof(double), (title, value) => EditorGUILayout.DoubleField(title, (double)value) },
+            { typeof(int), (title, value) => EditorGUILayout.IntField(title, (int)value) },
+            { typeof(long), (title, value) => EditorGUILayout.LongField(title, (long)value) },
+            { typeof(Vector2), (title, value) => EditorGUILayout.Vector2Field(title, (Vector2)value) },
+            { typeof(Vector3), (title, value) => EditorGUILayout.Vector3Field(title, (Vector3)value) },
+            { typeof(Vector4), (title, value) => EditorGUILayout.Vector4Field(title, (Vector4)value) },
+            { typeof(Vector2Int), (title, value) => EditorGUILayout.Vector2IntField(title, (Vector2Int)value) },
+            { typeof(Vector3Int), (title, value) => EditorGUILayout.Vector3IntField(title, (Vector3Int)value) },
+            { typeof(Rect), (title, value) => EditorGUILayout.RectField(title, (Rect)value) },
+            { typeof(RectInt), (title, value) => EditorGUILayout.RectIntField(title, (RectInt)value) },
+            { typeof(Bounds), (title, value) => EditorGUILayout.BoundsField(title, (Bounds)value) },
+            { typeof(BoundsInt), (title, value) => EditorGUILayout.BoundsIntField(title, (BoundsInt)value) },
+            { typeof(Color), (title, value) => EditorGUILayout.ColorField(title, (Color)value) },
+            { typeof(Gradient), (title, value) => EditorGUILayout.GradientField(title, (Gradient)value) },
+            { typeof(bool), (title, value) => EditorGUILayout.Toggle(title, (bool)value) },
+            { typeof(string), (title, value) => EditorGUILayout.TextField(title, (string)value) },
+        };
+
         public static void RefreshStatusBar(Action callback)
         {
             using (new EditorGUILayout.HorizontalScope(Styles.AppToolbar))
@@ -22,5 +45,49 @@ namespace UniFlux.Editor
                 }
             }
         }   
+
+        public static object SuperField(string title, Type type, object input)
+        {
+            object defaultValue;
+
+            if (type.IsValueType)
+            {
+                // If the parameter type is a value type or has a default constructor, create an instance
+                defaultValue = Activator.CreateInstance(type);
+            }
+            else
+            {
+                // Handle the case where the parameter type doesn't have a default constructor
+                defaultValue = null; // or provide a default value based on your requirements
+            }
+            // El parámetro es primitivo
+            object value = input ?? defaultValue;
+
+            // Verifica si el tipo está en el diccionario
+            if (editorFieldMap.ContainsKey(type))
+            {
+                // Obtiene la función correspondiente al tipo y la ejecuta
+                return editorFieldMap[type](title, value);
+            }
+            else if (type.IsInterface)
+            {
+                return EditorGUILayout.ObjectField(title, value as UnityEngine.Object, type, true);
+            }
+            else if (type.IsEnum)
+            {
+                return EditorGUILayout.EnumFlagsField(title, (Enum)value);
+            }
+            else if (type.IsClass)
+            {
+                // Si el tipo no está en el diccionario y no es un tipo de valor, usa ObjectField
+                return EditorGUILayout.ObjectField(title, value as UnityEngine.Object, type, true);
+            }
+            else
+            {
+                // EditorGUILayout.ser 
+                // Si no se reconoce el tipo, devuelve el valor sin modificar
+                return value;
+            }
+        }
     }
 }
